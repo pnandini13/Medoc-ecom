cfd5ebb7b4ebc3e505a639754c850dfabe2a98cb
Ansh3102 <anshulsharma2002.as@gmail.com>
2025-05-13 22:22:26 +0530

    Project done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,10 +4,20 @@ import { useSelector } from 'react-redux'
 
 const Navbar = () => {
     const state = useSelector(state => state.handleCart)
+    const isLoggedIn = localStorage.getItem('token') !== null
+    
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        window.location.reload() // Refresh to update the UI
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> React Ecommerce</NavLink>
+                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> 
+                    {/* Your brand logo/text here */}
+                </NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,7 +25,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto my-2 text-center">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Home </NavLink>
+                            <NavLink className="nav-link" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/product">Products</NavLink>
@@ -28,13 +38,25 @@ const Navbar = () => {
                         </li>
                     </ul>
                     <div className="buttons text-center">
-                        <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Login</NavLink>
-                        <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Cart ({state.length}) </NavLink>
+                        {isLoggedIn ? (
+                            <button onClick={handleLogout} className="btn btn-outline-dark m-2">
+                                <i className="fa fa-sign-in-alt mr-1"></i> Logout
+                            </button>
+                        ) : (
+                            <>
+                                <NavLink to="/login" className="btn btn-outline-dark m-2">
+                                    <i className="fa fa-sign-in-alt mr-1"></i> Login
+                                </NavLink>
+                                <NavLink to="/register" className="btn btn-outline-dark m-2">
+                                    <i className="fa fa-user-plus mr-1"></i> Register
+                                </NavLink>
+                            </>
+                        )}
+                        <NavLink to="/cart" className="btn btn-outline-dark m-2">
+                            <i className="fa fa-cart-shopping mr-1"></i> Cart ({state.length})
+                        </NavLink>
                     </div>
                 </div>
-
-
             </div>
         </nav>
     )
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,7 +5,9 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import {
   Home,
   Product,
